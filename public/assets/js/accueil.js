3f4de8eb0f89db32eac913b687ce699bbcaefd81
etsec-cloud <etienne.secondini@hotmail.fr>
2020-04-23 11:56:18 +0200

    modif style accueil

--- public/assets/js/accueil.js
+++ public/assets/js/accueil.js
@@ -2,8 +2,14 @@ console.log("azfe azef aze s")
 
 var swiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
-      slidesPerView: 6,
-      spaceBetween: 60,
-      loop: true,
-      
+    slidesPerView: 6,
+    spaceBetween: 60,
+    loop: true,
+    
+    autoplay: {
+      delay: 0,
+    },
+
+    speed: 3000,
+    
     });

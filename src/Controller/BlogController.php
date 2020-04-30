204a2f9fa33d7482ff9860801ed4341764fffbc6
etsec-cloud <etienne.secondini@hotmail.fr>
2020-04-30 02:03:07 +0200

    création d'article + liste

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Article;
+use App\Form\ArticleType;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -16,4 +19,47 @@ class BlogController extends AbstractController
             'controller_name' => 'BlogController',
         ]);
     }
+
+    /**
+     * @Route("/new", name="article_new", methods={"GET","POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $article->setCreatedAt(new \DateTime());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('blog/new.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/liste", name="article_liste")
+     */
+    public function liste( )
+    {
+        $em = $this->getDoctrine()->getManager();
+        $articles = $em->getRepository(Article::class)->findAll();
+
+        return $this->render('blog/liste.html.twig', [
+            'articles' => $articles
+        ]);
+
+       
+    }
+
+    
 }

f214224a476b01f3d007cf427128dd7fa851ad56
etsec-cloud <etienne.secondini@hotmail.fr>
2020-06-04 20:31:38 +0200

    page secteur

---
+++ public/assets/js/secteur.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container-secteur', {
+    direction: 'horizontal',
+    spaceBetween: 60,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });

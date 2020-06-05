c40474fa29983d93d3f472677cfb9901d7e0f88a
etsec-cloud <etienne.secondini@hotmail.fr>
2020-06-05 15:19:40 +0200

    responsive double pages

--- public/assets/js/articleList.js
+++ public/assets/js/articleList.js
@@ -1,10 +1,29 @@
 
 var swiper = new Swiper('.swiper-container-article', {
     direction: 'horizontal',
-    slidesPerView: 5,
-    spaceBetween: 60,
+    slidesPerView: 1,
+    spaceBetween: 30,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
+      breakpoints: {
+        400: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 20
+        },
+        700: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 30
+        },
+
+        1000: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 40
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetweenSlides: 60
+        }
+      }
     });
